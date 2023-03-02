9faf25fd6f4a56fe7989955005dfb28ea0fa12b6
LetTTGACO <1225751694@qq.com>
2023-03-02 19:16:22 +0800

    feat: rollup

--- packages/cli/rollup.config.js
+++ packages/cli/rollup.config.js
@@ -1,12 +1,13 @@
 import externals from 'rollup-plugin-node-externals'
 import commonjs from '@rollup/plugin-commonjs'
-import typescript from '@rollup/plugin-typescript'
 import json from '@rollup/plugin-json'
 import alias from '@rollup/plugin-alias'
-// import esbuild from 'rollup-plugin-esbuild'
-import pkg from './package.json'
-import path from 'path'
+import * as path from 'path'
 import resolve from '@rollup/plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript2'
+
+// const pkg = require('./package.json')
+import pkg from './package.json'
 
 export default [
   {
@@ -15,54 +16,45 @@ export default [
       dir: path.dirname(pkg.main),
       format: 'cjs',
       name: pkg.name,
-      exports: 'named', // 指定导出模式（自动、默认、命名、无）
-      preserveModules: true, // 保留模块结构
-      preserveModulesRoot: 'src', // 将保留的模块放在根级别的此路径下
     },
     plugins: [
+      typescript(),
+      json(),
+      commonjs(),
       resolve({
         preferBuiltins: true,
       }),
       alias(),
-      json(),
-      typescript({
-        outDir: 'dist/cjs',
-        declaration: true,
-        declarationDir: 'dist/cjs',
-      }),
       externals({
         devDeps: false, // devDependencies 类型的依赖就不用加到 externals 了。
       }),
-      commonjs(),
-      // esbuild(),
     ],
   },
-  {
-    input: 'src/index.ts',
-    output: {
-      dir: path.dirname(pkg.module),
-      format: 'es',
-      name: pkg.name,
-      exports: 'named', // 指定导出模式（自动、默认、命名、无）
-      preserveModules: true, // 保留模块结构
-      preserveModulesRoot: 'src', // 将保留的模块放在根级别的此路径下
-    },
-    plugins: [
-      resolve({
-        preferBuiltins: true,
-      }),
-      alias(),
-      json(),
-      typescript({
-        outDir: 'dist/esm',
-        declaration: true,
-        declarationDir: 'dist/esm',
-      }),
-      externals({
-        devDeps: false, // devDependencies 类型的依赖就不用加到 externals 了。
-      }),
-      commonjs(),
-      // esbuild(),
-    ],
-  },
+  // {
+  //   input: 'src/index.ts',
+  //   output: {
+  //     dir: path.dirname(pkg.module),
+  //     format: 'es',
+  //     name: pkg.name,
+  //     exports: 'named', // 指定导出模式（自动、默认、命名、无）
+  //     preserveModules: true, // 保留模块结构
+  //     preserveModulesRoot: 'src', // 将保留的模块放在根级别的此路径下
+  //   },
+  //   plugins: [
+  //     json(),
+  //     esbuild({
+  //       loaders: {
+  //         '.json': 'json',
+  //       },
+  //     }),
+  //     resolve({
+  //       preferBuiltins: true,
+  //     }),
+  //     alias(),
+  //     commonjs(),
+  //     externals({
+  //       devDeps: false, // devDependencies 类型的依赖就不用加到 externals 了。
+  //     }),
+  //   ],
+  // },
 ]
